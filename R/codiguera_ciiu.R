cec680a7aecf0eeea131aeb8dc5a78f6aa69e8d4
Virginia Burguete <vburguete@transformauruguay.gub.uy>
2018-12-27 12:24:44 -0300

    agrega paquete

--- R/codiguera_ciiu.R
+++ R/codiguera_ciiu.R
@@ -24,25 +24,25 @@ codiguera_ciiu <- function(df){
 
   seccion %>%
 
-    left_join(seccion_division, by = "seccion") %>%
+    dplyr::left_join(seccion_division, by = "seccion") %>%
 
-    left_join(division, by = "division") %>%
+    dplyr::left_join(division, by = "division") %>%
 
-    left_join(grupo %>% mutate(division = str_sub(grupo, 1, 2)), by = "division") %>%
+    dplyr::left_join(grupo %>% mutate(division = str_sub(grupo, 1, 2)), by = "division") %>%
 
-    left_join(clase %>% mutate(grupo = str_sub(clase, 1, 3)), by = "grupo") %>%                              # ciiu a 4 dígitos
-    mutate(clase = case_when(is.na(clase) == TRUE ~ str_c(grupo, 0), TRUE ~ clase),
+    dplyr::left_join(clase %>% mutate(grupo = str_sub(clase, 1, 3)), by = "grupo") %>%                              # ciiu a 4 dígitos
+    dplyr::mutate(clase = case_when(is.na(clase) == TRUE ~ str_c(grupo, 0), TRUE ~ clase),
            desc_clase = case_when(is.na(desc_clase) == TRUE ~ desc_grupo, TRUE ~ desc_clase)) %>%
 
-    left_join(subclase %>% mutate(clase = str_sub(subclase, 1, 4),                                           # ciiu a 5 dígitos
+    dplyr::left_join(subclase %>% mutate(clase = str_sub(subclase, 1, 4),                                           # ciiu a 5 dígitos
                                   grupo = str_sub(subclase, 1, 3)),
               by = c("clase", "grupo")) %>%
-    mutate(subclase = case_when(is.na(subclase) == TRUE ~ str_c(clase, 0), TRUE ~ subclase),
+    dplyr::mutate(subclase = case_when(is.na(subclase) == TRUE ~ str_c(clase, 0), TRUE ~ subclase),
            desc_subclase = case_when(is.na(desc_subclase) == TRUE ~ desc_clase, TRUE ~ desc_subclase)) %>%
-    mutate(desc_clase = if_else(clase == "4631", "Comercio al por mayor de alimentos, bebidas y tabaco.",
+    dplyr::mutate(desc_clase = if_else(clase == "4631", "Comercio al por mayor de alimentos, bebidas y tabaco.",
                                 desc_clase)) %>%
 
-    union_all(cargar_ciiu_INE_anexo) %>%
+    dplyr::union_all(cargar_ciiu_INE_anexo) %>%
     unique
 }
 
